69b6266f9de9d5714251c36f82a6122a2502332d
Roman Novikov <mystdeim@gmail.com>
2017-02-07 22:10:50 +0200

    fix cache

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
 import io.vertx.core.Vertx;
 
+import static java.lang.System.out;
+
 /**
  * @author romanovi
  * @since 2/7/17.
  */
 public class Main {
 
+    // -Dvertx.disableFileCaching=true
     public static void main(String[] args) {
+
+        System.getProperties().entrySet().stream()
+                .filter(p -> p.getKey().toString().startsWith("vertx"))
+                .forEach(p -> out.printf("%s=%s\n", p.getKey(), p.getValue()));
+
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(new FrontendVerticle());
     }
